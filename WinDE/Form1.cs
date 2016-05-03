cf57e512487b200d4b80903282c3e3f481e75155
sinyu1012 <1341156974@qq.com>
2016-05-03 22:37:55 +0800

    C# DataGridView 导出到Excel 的Demo
    C# DataGridView 导出到Excel 的Demo

---
+++ WinDE/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WinDE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ExportDataGridview(dataGridView1, true))
+                MessageBox.Show("导出成功，请记得保存!");
+            else
+                MessageBox.Show("导出未成功，请检查是否有错!");
+        }
+        public bool ExportDataGridview(DataGridView gridView, bool isShowExcle)//生成Excel
+        {
+            if (gridView.Rows.Count == 0)
+                return false;
+            //建立Excel对象
+            Excel.Application excel = new Excel.Application();
+            excel.Application.Workbooks.Add(true);
+            excel.Visible = isShowExcle;
+            //生成字段名称
+            for (int i = 0; i < gridView.ColumnCount; i++)
+            {
+                excel.Cells[1, i + 1] = gridView.Columns[i].HeaderText;
+            }
+            //填充数据
+            for (int i = 0; i < gridView.RowCount; i++)
+            {
+                for (int j = 0; j < gridView.ColumnCount; j++)
+                {
+                    if (gridView[j, i].ValueType == typeof(string))
+                    {
+                        excel.Cells[i + 2, j + 1] = "'" + gridView[j, i].Value.ToString();
+                    }
+                    else
+                    {
+                        excel.Cells[i + 2, j + 1] = gridView[j, i].Value.ToString();
+                    }
+                }
+            }
+            return true;
+        }
+
+       
+    }
+}
